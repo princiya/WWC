4f3f6b7e57953e1babe695358c692b6df78a2fb4
Princiya <princiya.m@guru-g.com>
2015-07-27 14:33:18 +0530

    First commit

---
+++ sdk/hotfix/README.txt
@@ -0,0 +1,2 @@
+Apply this hot fix if you see instability of the JavaScript* backend service.
+Run hotfix_web_server.cmd as admin to apply the hot fix.
---
+++ sdk/hotfix/hotfix_web_server.cmd
@@ -0,0 +1,7 @@
+taskkill /F /IM RealSenseWeb.exe
+
+copy /Y RealSenseWeb.exe "C:\Program Files (x86)\Common Files\Intel\RSSDK\v4\bin\win32\RealSenseWeb\"
+
+
+START /B "" "C:\Program Files (x86)\Common Files\Intel\RSSDK\v4\bin\win32\RealSenseWeb\RealSenseWeb.exe"
+
